5e7988fdd3c190d31f53995e6f9bf3b3cbe424d2
ASHISH PAL <ashishpl@cse.iitk.ac.in>
2020-04-10 00:55:46 +0530

    Added experiments

--- Experiments/README.md
+++ Experiments/README.md
@@ -1,5 +1,11 @@
-Experiment - 1 => Traning and testing on 50000 selected jobs with 20-80 split for training and testing respectively.
+Experiment - 1 => Traning and testing on 50000 selected jobs with 20-80 split for training and testing respectively. Using 5-fold cross validation.
 
-Experiment - 2 => Same as Experiemnt 1 but this time using 5-fold cross validation.
+Experiment - 2 => Traning and testing on 50000 selected jobs with 20-80 split for training and testing respectively.
+
+Experiment - 3 => Traning and testing on 50000 selected jobs with 40-60 split for training and testing respectively.
+
+Experiment - 4 => Traning and testing on 50000 selected jobs with 60-40 split for training and testing respectively.
+
+Experiment - 5 => Traning and testing on 50000 selected jobs with 80-20 split for training and testing respectively.
 
 
--- code/calcWtime.py
+++ code/calcWtime.py
@@ -4,27 +4,28 @@ import sys
 import json
 
 
-# # Load the model from the file 
-# kmeans = joblib.load('/var/www/html/new/predict_models/Kmeans.pkl')  
+# Load the model from the file 
+kmeans = joblib.load('/var/www/html/new/predict_models/Kmeans.pkl')  
 
-# reg_models = []
-# for i in range(6):
-#     reg_models.append(joblib.load('/var/www/html/new/predict_models/Log_reg'+str(i)+'.pkl'))
+reg_models = []
+for i in range(6):
+    reg_models.append(joblib.load('/var/www/html/new/predict_models/Log_reg'+str(i)+'.pkl'))
  
-# QState = {}
-# with open('/var/www/html/new/realtime/2010/QState.json','r') as fp:
-#     QState = json.load(fp)
+QState = {}
+with open('/var/www/html/new/realtime/2010/QState.json','r') as fp:
+    QState = json.load(fp)
 
-# #print (sys.argv[1],sys.argv[2],sys.argv[3])
-# nodes = int(sys.argv[1])
-# walltime = int(sys.argv[2])
-# queue = str(sys.argv[3])
+#print (sys.argv[1],sys.argv[2],sys.argv[3])
+nodes = int(sys.argv[1])
+walltime = int(sys.argv[2])
+queue = str(sys.argv[3])
 
-# test = [nodes*walltime/11520,QState['qstate'][queue]/5,QState['status']['R']/466]
-# predicted_cluster = kmeans.predict([test])[0]
-# predicted_wt = reg_models[predicted_cluster].predict([test])[0]
-# print (predicted_wt)
+test = [nodes*walltime/11520,QState['qstate'][queue]/5,QState['status']['R']/466]
+predicted_cluster = kmeans.predict([test])[0]
+predicted_wt = reg_models[predicted_cluster].predict([test])[0]
+print (predicted_wt)
 
+'''
 output = ''
 #For XGB
 # Load the model from the file 
@@ -90,3 +91,4 @@ predicted_wt = reg_models[predicted_cluster].predict([test])[0]
 output += str(predicted_wt)
 
 print (output)
+'''
