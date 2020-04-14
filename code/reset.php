500568ff7b44675137ef0dbf75cdfc3cc685e8c7
ASHISH PAL <ashishpl@cse.iitk.ac.in>
2020-04-14 21:20:13 +0530

    Cleaned

--- code/reset.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-if (!unlink($_POST['file'])) {  
-    echo ("$_POST['file'] cannot be deleted due to an error");  
-}  
-else {  
-    echo ("$_POST['file'] has been deleted");  
-}  
-?>
